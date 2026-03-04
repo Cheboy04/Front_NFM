887c9ff72e795dd8ea260e4d35e5d9e65727f785
Cheboy04 <cornejoadrian93@gmail.com>
2026-03-04 12:01:21 -0500

    Fix Icons not loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className="dark">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+        />
+      </head>
       <body className={`${spaceGrotesk.variable} ${lora.variable} font-display antialiased min-h-screen flex flex-col`}>
         <DraftAlert />
         <div className="flex-1">
